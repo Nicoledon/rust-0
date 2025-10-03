50e9e8e78edbd8aa7cf0992815900ec1def26ad0
Nicoledon <3121506708@qq.com>
2025-10-03 20:36:27 +0800

    pa2

--- part1/src/lib.rs
+++ part1/src/lib.rs
@@ -1,8 +1,20 @@
+use std::cmp;
+
 /// Returns the sqrt of `n`
 ///
 /// Using built-in square root functions is not allowed.
-pub fn sqrt(n: u32) -> u32 {
-    todo!();
+pub fn sqrt(n: u32) -> u32 { 
+    if n == 0 {
+        return 0;
+    }
+    for i in 1..n{
+         if i * i > n {
+             return i - 1;
+         }else if i * i == n {
+             return i ;
+         }
+    }
+    return 1;
 }
 
 /// Consumes a sorted list of integers and a query integer. Returns the index of the query integer
@@ -15,8 +27,20 @@ pub fn sqrt(n: u32) -> u32 {
 ///    std::cmp::Ordering::Equal => { ... },
 /// }
 /// ```
+
 pub fn binary_search(arr: &[i32], query: i32) -> Option<u32> {
-    todo!();
+     return binary_search_helper(arr, query , 0 as usize);
+}
+pub fn binary_search_helper(arr: &[i32], query: i32 , count :usize ) -> Option<u32> {
+    if arr.len() == 0{
+         return None;
+    }
+    let mid = (0 + (arr.len())) / 2;
+    match query.cmp(&arr[mid]) {
+        std::cmp::Ordering::Less => {return binary_search_helper(&arr[0..mid], query , count)},
+        std::cmp::Ordering::Greater => { return  binary_search_helper(&arr[mid + 1 ..arr.len()], query , count + mid + 1)},
+        std::cmp::Ordering::Equal => {Some((mid + count)  as u32)},
+    }
 }
 
 /// Consumes a list of numbers representing daily rainfall. The list may contain -999 signifying
